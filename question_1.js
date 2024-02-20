d0e01bc7ebc7cd2c8d3feedbee437e03ee9ac229
Shahzaib Meraj <140791911+Shabi4023@users.noreply.github.com>
2024-02-20 12:31:27 +0500

    Add files via upload

---
+++ question_1.js
@@ -0,0 +1,6 @@
+// Write a JavaScript program to create an array named ‘colors’ and add ‘Red’, ‘Blue’, and ‘Green’ to it.Access the second element of the array and print it.
+
+
+const colors = ["Red", "Blue", "Green"];
+let result = colors.at(1);
+console.log(result);
